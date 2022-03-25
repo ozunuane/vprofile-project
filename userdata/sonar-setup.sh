4a6d0aa1e46124a941b5c50bfe748dfb3145cb15
Ozimede <30769770+ozunuane@users.noreply.github.com>
2022-03-25 11:12:17 +0100

    Update sonar-setup.sh

--- userdata/sonar-setup.sh
+++ userdata/sonar-setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 cp /etc/sysctl.conf /root/sysctl.conf_backup
-cat <<EOT> /etc/sysctl.conf
+cat << EOT > /etc/sysctl.conf
 vm.max_map_count=262144
 fs.file-max=65536
 ulimit -n 65536
 ulimit -u 4096
 EOT
 cp /etc/security/limits.conf /root/sec_limit.conf_backup
-cat <<EOT> /etc/security/limits.conf
+cat <<EOT > /etc/security/limits.conf
 sonarqube   -   nofile   65536
 sonarqube   -   nproc    409
+
 EOT
 
 sudo apt-get update -y
@@ -43,7 +44,7 @@ sudo groupadd sonar
 sudo useradd -c "SonarQube - User" -d /opt/sonarqube/ -g sonar sonar
 sudo chown sonar:sonar /opt/sonarqube/ -R
 cp /opt/sonarqube/conf/sonar.properties /root/sonar.properties_backup
-cat <<EOT> /opt/sonarqube/conf/sonar.properties
+cat <<EOT > /opt/sonarqube/conf/sonar.properties
 sonar.jdbc.username=sonar
 sonar.jdbc.password=admin123
 sonar.jdbc.url=jdbc:postgresql://localhost/sonarqube
@@ -55,7 +56,7 @@ sonar.log.level=INFO
 sonar.path.logs=logs
 EOT
 
-cat <<EOT> /etc/systemd/system/sonarqube.service
+cat << EOT > /etc/systemd/system/sonarqube.service
 [Unit]
 Description=SonarQube service
 After=syslog.target network.target
@@ -85,7 +86,7 @@ systemctl enable sonarqube.service
 apt-get install nginx -y
 rm -rf /etc/nginx/sites-enabled/default
 rm -rf /etc/nginx/sites-available/default
-cat <<EOT> /etc/nginx/sites-available/sonarqube
+cat <<EOT > /etc/nginx/sites-available/sonarqube
 server{
     listen      80;
     server_name sonarqube.groophy.in;
